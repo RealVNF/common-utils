d8cb4a5c9d19647a64f317c3a6c2bebad253d6b2
Adnan Manzoor <adnan904@mail.uni-paderborn.de>
2019-07-01 18:30:59 +0200

    Travis error fix

--- src/common/common_functionalities.py
+++ src/common/common_functionalities.py
@@ -1,13 +1,9 @@
-from typing import no_type_check
-
-
 # rounds off the sum of all probabilities in the list to be equal to 1.0
 # the input list maybe manipulated to make the sum equal to 1
 # an error range of +-0.0000152587890625 in the sum has to be handled at the place of function calling.
 
 
-@no_type_check
-def round_off_list_to_1(input_list: 'list') -> 'manipulates the input list object itself':
+def round_off_list_to_1(input_list: 'list'):
     offset = 1 - sum(input_list)
     # Because of floating point precision (.59 + .33 + .08) can be equal to .99999999
     # So we correct the sum only if the absolute difference is more than a tolerance(0.0000152587890625)
@@ -16,8 +12,8 @@ def round_off_list_to_1(input_list: 'list') -> 'manipulates the input list objec
         # we divide each number in the list by the sum of the list, so that Prob. Distribution is approx. 1
         for i in range(len(input_list)):
             input_list[i] = round(input_list[i] / sum_list, 2)
+        # 1 - sum(input_list) = the diff. by which the elements of the list are away from 1.0, could be +'ive /-i've
         new_offset = 1 - sum(input_list)
-        # 1 - sum(prob_list) = the difference by which the elements of the list are away from 1.0, could be +'ive /-i've
         # the difference is added/subtracted from the 1st element of the list, which is also rounded to 2 decimal points
         input_list[0] = round(input_list[0] + new_offset, 2)
     # to handle the empty list case
