1118bd4d3e9857dd53240f8f153dd879d42d8f9f
Adnan Manzoor <adnan904@gmail.com>
2020-02-06 18:45:18 +0100

    Travis fix

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
+os.system('pip install git+https://github.com/RealVNF/coord-env-interface.git')
 os.system('pip install git+https://github.com/RealVNF/coordination-simulation.git')
 
 requirements = [
